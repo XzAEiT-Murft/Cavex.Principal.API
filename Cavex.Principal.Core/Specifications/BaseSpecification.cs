800516d247b80b25e48a4080dc0a1d3ffc15cbf9
c0d3x <xzaeitmurft@gmail,com>
2026-06-02 19:47:19 -0600

    feat: se agregan cambios a la arquitectura base fix: se elimino la Class1 que se habia creado de ejemplo

---
+++ Cavex.Principal.Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cavex.Principal.Core.Specifications
+{
+    public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecification<T>
+    {
+        //podemos especificar q el constructor puede recibir un valor null y de esta manera ser mas flexible al
+        //paso de parametros.
+        protected BaseSpecification() : this(null) { }
+        //private readonly Expression<Func<T, bool>> criteria = criteria;
+        public Expression<Func<T, bool>>? Criteria => criteria;
+        /// <summary>
+        /// comentando para que no de error de compilacion
+        /// </summary>
+        public Expression<Func<T, object>>? OrderBy { get; private set; }
+
+        public Expression<Func<T, object>>? OrderByDescending { get; private set; }
+
+        public bool IsDistinct { get; private set; }
+
+        public int Take { get; private set; }
+
+        public int Skip { get; private set; }
+
+        public bool IsPagingEnabled { get; private set; }
+
+        public IQueryable<T> ApplyCriteria(IQueryable<T> query)
+        {
+            if (Criteria is not null)
+            {
+                query = query.Where(Criteria);
+            }
+            return query;
+        }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
+        }
+
+        protected void ApplyDistinct()
+        {
+            IsDistinct = true;
+        }
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
+        }
+
+
+    }
+
+    public class BaseSpecification<T, TResult>(Expression<Func<T, bool>> criteria) :
+        BaseSpecification<T>(criteria), ISpecification<T, TResult>
+    {
+        protected BaseSpecification() : this(null!) { }
+        public Expression<Func<T, TResult>>? Select { get; private set; }
+
+        protected void AddSelect(Expression<Func<T, TResult>> selectExpression)
+        {
+            Select = selectExpression;
+        }
+    }
+
+}
