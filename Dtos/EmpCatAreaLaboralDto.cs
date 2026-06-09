61e58699d9c5f2f1b735f234c0b61c52be7da91f
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-06-09 12:36:35 -0600

    feat:se agrega la clase EmpCatAreaLaboralDto  con los atributos necesarios

---
+++ Dtos/EmpCatAreaLaboralDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Cavex.Principal.API.Dtos
+{
+    public class EmpCatAreaLaboralDto
+    {
+        public int Id { get; set; }
+
+        public string StrValor { get; set; } = string.Empty;
+
+        public string? StrDescripcion { get; set; }
+    }
+}
